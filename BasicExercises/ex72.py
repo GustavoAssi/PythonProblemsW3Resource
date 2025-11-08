75dab6488e959ac3de6922d8f76dab5caeac073c
Gustavo <alencarassigustavo@gmail.com>
2025-11-08 09:03:29 -0400

    Commit 15 - More solved exercises
    - List of changes:
        - Solved ex71, ex72, ex73, ex74 and ex75.

---
+++ BasicExercises/ex72.py
@@ -0,0 +1,33 @@
+import math
+
+
+from utils import Input
+
+
+def math_module_details():
+	"""
+	This program get the details of the math module.
+	Author: Gustavo Assi Alencar.
+	Date:   06/11/2025.
+	"""
+
+	# Step 1: Display math module functions attributes (object).
+	print("All math functions and atributes.")
+	math_module_objects = dir(math)
+	for object in math_module_objects:
+		print(f">>> {object}")
+
+	# Step 2: Ask for user to use help from one of the given objects list.
+	user_query = Input.get_string("Type an object to get more informations: ", stripped=True)
+	if user_query in math_module_objects:
+		exec(f"help(math.{user_query})")
+	else:
+		print(f'Couldn\'t find "{user_query}" defined in math module.')
+
+
+def main():
+	math_module_details()
+
+
+if __name__ == "__main__":
+	main()
---
+++ BasicExercises/ex75.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def copyright_information():
+	"""
+	This program get the copyright information and write Copyright information in Python code.
+	Author: Gustavo Assi Alencar.
+	Date:   06/11/2025.
+	"""
+
+	# Step 1: Display copyright information.
+	print(sys.copyright)
+
+
+def main():
+	copyright_information()
+
+
+if __name__ == "__main__":
+	main()
